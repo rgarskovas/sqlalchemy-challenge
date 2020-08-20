95c5691388b43e354e549e9d750faadedd808393
rgarskovas <65867325+rgarskovas@users.noreply.github.com>
2020-08-20 17:30:59 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -59,6 +59,7 @@ def prcp():
 
     weather_data = []
 
+    # create a dictionary for the data    
     for date, prcp in results:
         weather_dict = {}
         weather_dict["date"] = date
@@ -80,9 +81,9 @@ def station():
     """Return a list of all stations"""
     # Query all stations
     results = session.query(measurement.station).all()
-
     StationsDF = pd.DataFrame(results,columns = ['stations',])
 
+    # limit to unique stations
     unique_stations = StationsDF["stations"].unique()
 
     session.close()
@@ -104,11 +105,9 @@ def tobs():
     # Calculate the date 1 year ago from the last data point in the database
 
     last_date = session.query(measurement.date).order_by(measurement.date.desc()).first()[0]
-
     previous_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
 
     # Perform a query to retrieve the data and precipitation scores
-
     results = session.query(measurement.date, measurement.prcp).filter(measurement.date > previous_date).all()
 
     session.close()
@@ -126,14 +125,18 @@ def starting_date(start):
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
+    # Create sel function for min, max average
     sel = [func.min(measurement.tobs), 
        func.max(measurement.tobs), 
        func.avg(measurement.tobs),]
 
+    # take variable from input
     start_date = start
 
+    # make the query with filters
     results = session.query(*sel).filter(measurement.date > start_date).all()
 
+    # close the session
     session.close()
 
     # Convert list of tuples into normal list
@@ -147,15 +150,19 @@ def date_range(start, end):
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
+    # Create sel function for min, max average
     sel = [func.min(measurement.tobs),
        func.max(measurement.tobs), 
        func.avg(measurement.tobs),]
 
+    # take variables from input
     start_date = start
     end_date = end
 
+    # make the query with filters
     results = session.query(*sel).filter(measurement.date < end_date).filter(measurement.date > start_date).all()
 
+    # close the session
     session.close()
 
     # Convert list of tuples into normal list
